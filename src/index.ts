535eb64c18b7c32124b670a9f8eedf5e3cbc7519
Fadli Ramadhan <fadli16ramadhan@gmail.com>
2025-11-20 23:41:37 +0700

    Report (#4)
    * auth login for user
    
    * crud lead
    
    * crud product
    
    * create project and customer service
    
    * add detail
    
    * fix cors

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,17 @@ import cors from "cors"
 import authRoute from "~/routes";
 import { logger } from "~/middlewares/logger";
 import { errorMiddleware } from "~/middlewares/error.middleware";
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
-const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || [];
+const port = Number(process.env.PORT)
+const allowedCors = process.env.CORS_ORIGIN
 
 app.use(cors({
-    origin: allowedOrigins
-}));
+    origin: allowedCors
+}))
 
 app.use(express.json());
 
@@ -23,6 +24,6 @@ app.use("/crm-api", authRoute);
 
 app.use(errorMiddleware);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running at http://localhost:${port}`);
 });
